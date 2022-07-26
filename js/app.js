997650a512b3d0adb33e944e7d4d8a0f41c42105
A.Adefolalu B <adefolalucode101@gmail.com>
2022-07-26 19:10:15 +0100

    add javascript code to multiply a number in an array

---
+++ js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    // array number to multiply
+    let multiTable = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+   
+
+    // multiply 1 by our array
+    multiTable.forEach(function(multiply){
+        let num = 1;
+        return(multiply * num);
+
+    });
+    multiTable;
+
+
+
+
+
+
+
+
+
+});
