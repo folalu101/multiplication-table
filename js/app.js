aa8713dc2b3070c951c0ec7c6e4896dd5f81895a
A.Adefolalu B <adefolalucode101@gmail.com>
2022-07-30 20:41:44 +0100

    add more javascript code to js file to perform more loop from our array

--- js/app.js
+++ js/app.js
@@ -1,16 +1,49 @@
 $(document).ready(function(){
     // array number to multiply
     let multiTable = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-   
+    let newTable = [];
 
     // multiply 1 by our array
+    // multiTable.forEach(function(multiply){
+    //     let num = 1;
+    //     newTable.push(multiply * num);
+    // });
+    // console.log(newTable);
+
+    //  multiply 2 by our array
     multiTable.forEach(function(multiply){
-        let num = 1;
-        return(multiply * num);
+        let num = 2;
+        newTable.push(multiply * num);
 
     });
-    multiTable;
+    document.write(newTable);
 
+    // multiply 3 by our array
+    multiTable.forEach(function(multiply){
+        let num = 3;
+        newTable.push(multiply * num);
+    });
+    console.log(newTable);
+
+    // multiply 4 by our array
+    multiTable.forEach(function(){
+        let num = 4;
+        newTable.push(multiply * 4);
+    });
+    console.log(newTable);
+
+    // multiply 5 by our array
+    multiTable.forEach(function(){
+        let num = 5;
+        newTable.push(multiply * 5);
+    });
+    console.log(newTable);
+
+
+
+   
+
+   
 
 
 
